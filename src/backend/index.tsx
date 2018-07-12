1973278651ca35afc4094469d0426def749d37e8
darvid7 <david.anthony.lei@gmail.com>
2018-07-12 19:52:19 +1000

    Fix some imports & env stuff

--- src/backend/index.tsx
+++ src/backend/index.tsx
@@ -1,15 +1,18 @@
 import 'source-map-support/register';
 
 // import { initDummyData } from 'backend/initDummyData';
-import { logger, overrideConsoleLogger, overrideUtilInspectStyle } from 'backend/logger';
 import { createServer } from 'backend/server';
 
+import { logger, overrideConsoleLogger, overrideUtilInspectStyle } from 'backend/logger';
 import { config } from 'config';
-overrideConsoleLogger(logger);
-overrideUtilInspectStyle();
+
 import 'source-map-support/register';
 
 async function run(port: number) {
+  // Overriding logger used in non testing environments, logging in tests just go to stdout.
+  overrideConsoleLogger(logger);
+  overrideUtilInspectStyle();
+
   const app = await createServer();
   app.listen(port, (er, err) => {
     logger.info(`Swagger Platform Server now listening on port ${port}`);
