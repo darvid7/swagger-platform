2f99d6c94f0a0a8f1a204af64c8f04f130ac55e9
darvid7 <david.anthony.lei@gmail.com>
2018-07-12 18:39:54 +1000

    Refactor out server

--- webpack.config.js
+++ webpack.config.js
@@ -68,6 +68,7 @@ module.exports = (env, argv) => {
                   regenerator: true,
                 },
               ],
+              ['@babel/plugin-proposal-export-default-from'],
             ],
           },
         },
