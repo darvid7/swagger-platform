0b4ba4a3509df17ae82bf4b09124c51b5e3d63e1
darvid7 <david.anthony.lei@gmail.com>
2018-07-13 16:50:16 +1000

    Add server tests

--- src/client/sdkGeneration.tsx
+++ src/client/sdkGeneration.tsx
@@ -5,11 +5,11 @@ import { Spec } from 'model/Spec';
 
 const SWAGGER_CODEGEN_ENDPOINT = 'http://generator.swagger.io/api/gen/clients/';
 
-export async function generateSdk(logger, spec: Spec, plan: Plan): Promise<any> {
-  /* Generates an SDK for a Spec
-  * @param {Spec} spec - The Spec object for which the sdk needs to be generated
-  * @return {Promise<string>} - The URL from which the sdk can be downloaded
-  */
+/* Generates an SDK for a Spec
+* @param {Spec} spec - The Spec object for which the sdk needs to be generated
+* @return {Promise<string>} - The URL from which the sdk can be downloaded
+*/
+export async function generateSdk(logger, spec: Spec, plan: Plan): Promise<string> {
   const body = { swaggerUrl: spec.path, options: plan.options };
   const response = await fetch(SWAGGER_CODEGEN_ENDPOINT + plan.target, {
     method: 'POST',
@@ -17,7 +17,7 @@ export async function generateSdk(logger, spec: Spec, plan: Plan): Promise<any> 
     headers: { 'Content-Type': 'application/json' },
   });
   const json = await response.json();
-  if (json.type === 'error') {
+  if (json.type === 'error' || json.type === 'unknown') {
     throw new Error(json.message);
   }
   return json;
